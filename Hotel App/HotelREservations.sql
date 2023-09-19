15cdb95124811ed13bd50b4bf89285ba7a164623
Cafer Altiparmak <caferaltiparmak@icloud.com>
2023-09-19 10:37:26 +0300

    Update HotelREservations.sql

--- Hotel App/HotelREservations.sql
+++ Hotel App/HotelREservations.sql
@@ -1,8 +1,7 @@
-/*CREATE TABLE HotelReservation (
+CREATE TABLE HotelReservation (
     HotelID INT,
     MemberName NVARCHAR(255),
     Room NVARCHAR(50),
     Day INT,
     ReleaseDate DATE
 );
-*/
